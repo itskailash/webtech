8fee76954ff5faf4dd41a91783823884fdf2b96d
itskailash <kailash.wit@gmail.com>
2023-09-16 13:56:35 +0545

    Web Technology Report

---
+++ lab8/footer.php
@@ -0,0 +1,3 @@
+<footer>
+    <p>&copy; <?php echo date('Y'); ?> My Simple Website</p>
+</footer>
---
+++ lab8/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<head>
+    <title>My Simple Website</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php include('header.php'); ?>
+    
+    <div class="content">
+        <h1>Welcome to My Website</h1>
+        <p>This is a simple PHP-based website.</p>
+    </div>
+
+    <?php include('footer.php'); ?>
+</body>
+</html>
